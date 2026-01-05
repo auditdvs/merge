387266f3ced5326e6c44d0c1daeac1ae01f845f7
auditdvs <dvsaudit@gmail.com>
2026-01-05 12:06:08 +0700

    Update merge-dbcr.py

--- merge-dbcr.py
+++ merge-dbcr.py
@@ -4,7 +4,16 @@ import numpy as np
 import io
 
 st.title('Aplikasi Penggabungan Simpanan dan Pinjaman')
-st.write("""1. Format file harus bernama dan menggunakan ekstensi csv di excelnya pilih save as *CSV UTF-8 berbatas koma atau coma delimited*, sehingga seperti ini : THC.csv, DbPinjaman.csv, DbSimpanan.csv""")
+st.divider()
+st.caption("Catatan:")
+st.caption("Pengolahan ini digunakan untuk menyatukan antara simpanan dan pinjaman")
+st.caption("Dipastikan pivot_pinjaman_na.xlsx :red[**isinya sudah disatukan**] dengan pivot_pinjaman.xlsx")
+st.divider()
+
+st.subheader("File Yang Dibutuhkan:")
+st.write("1. pivot_simpanan.xlsx")
+st.write("2. pivot_pinjaman.xlsx")
+
 
 uploaded_files = st.file_uploader("Unggah file Excel", accept_multiple_files=True, type=["xlsx"])
 
@@ -16,16 +25,18 @@ if uploaded_files:
 
 
 
-# Konversi data menjadi DataFrame
-df_s = pd.DataFrame(pivot_simpanan)
-df_p = pd.DataFrame(pivot_pinjaman)
+    # Konversi data menjadi DataFrame
+    if 'pivot_simpanan.xlsx' in dfs:
+        df_s = dfs['pivot_simpanan.xlsx']
+    if 'pivot_pinjaman.xlsx' in dfs:
+        df_p = dfs['pivot_pinjaman.xlsx']
 
-dfs_merged = pd.merge(df_s, df_p[['DUMMY', 'Db PTN', 'Cr PTN', 'Db PRT', 'Cr PRT', 'Db DTP', 'Cr DTP', 'Db PMB', 'Cr PMB', 'Db PRR', 'Cr PRR',
+    dfs_merged = pd.merge(df_s, df_p[['DUMMY', 'Db PTN', 'Cr PTN', 'Db PRT', 'Cr PRT', 'Db DTP', 'Cr DTP', 'Db PMB', 'Cr PMB', 'Db PRR', 'Cr PRR',
             'Db PSA', 'Cr PSA', 'Db PU', 'Cr PU', 'Db Total2', 'Cr Total2']], on='DUMMY', how='left')
-dfs_merged = dfs_merged.fillna(0)
+    dfs_merged = dfs_merged.fillna(0)
 
-dfs_merged['CENTER'] = dfs_merged['CENTER'].astype(str).str.zfill(3)
-dfs_merged['KELOMPOK'] = dfs_merged['KELOMPOK'].astype(str).str.zfill(2)
+    st.write("THC Final setelah di proses:")
+    st.write(dfs_merged)
 
 # Download links for pivot tables
     for name, df in {
